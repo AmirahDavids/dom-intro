5c8118cd77107d56a4fb1574ecc761b2ebf8d27b
AmirahDavids <amirahdavids2503@gmail.com>
2020-04-08 15:25:09 +0200

    added functionality to calculate-bill widget

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,13 +1,32 @@
-//get a reference to the calculate button
+var calculateBtnElement = document.querySelector(".calculateBtn");
+var billTotalElement = document.querySelector(".billTotal");
+var billStringElement = document.querySelector(".billString");
 
-//get a reference to the billTotal element
-
-//get a reference to the billString
-
-//create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
-
-//link the function to a click event on the calculate button
+function calculateBtnClicked() {
+    var total = 0.00;
+    var billString = billStringElement.value;
+    var list = billString.split(',');
+    for (var i = 0; i < list.length; i = i + 1) {
+        var action = list[i].trim();
+        switch (action) {
+            case "call":
+                total += 2.75;
+                break;
+            case "sms":
+                total += 0.75;
+                break;
+        };
+    };
+    if (total > 20 && total < 30) {
+        billTotalElement.classList.remove("danger");
+        billTotalElement.classList.add("warning");
+    } else if (total >= 30) {
+        billTotalElement.classList.remove("warning");
+        billTotalElement.classList.add("danger");
+    } else {
+        billTotalElement.classList.remove("warning");
+        billTotalElement.classList.remove("danger");
+    }
+    billTotalElement.innerHTML = total.toFixed(2);
+}
+calculateBtnElement.addEventListener('click', calculateBtnClicked);
